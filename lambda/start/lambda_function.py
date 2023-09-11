79f08266b078ce23a27114469c82eed65d4672a8
Jack Curran <jackurran49@gmail.com>
2023-09-11 16:00:56 -0400

    first commit

---
+++ lambda/start/lambda_function.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+
+ec2 = boto3.client('ec2', region_name='us-east-1')
+invoker = boto3.client('lambda', region_name='us-east-1')
+
+def lambda_handler(event, context):
+    instance_id = os.environ.get('INSTANCE_ID')
+    domain = os.environ.get('DOMAIN')
+    dns_function = os.environ.get('DNS_FUNCTION')
+
+    ec2.start_instances(InstanceIds=[instance_id])    
+    
+    invoker.invoke(
+        FunctionName=dns_function,
+        InvocationType='Event'
+    )
+    
+    msg = 'Minecraft server is starting up, and in a few moments it will be ' + \
+            'accessible at ' + + domain + '. ' + \
+            '\nFor now, check the /status endpoint to track whether the server is up and running.'
+    msg
+    return {
+        'statusCode': 200,
+        'body': msg
+    }
