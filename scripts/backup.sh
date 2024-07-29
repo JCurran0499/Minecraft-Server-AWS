c4bf5a3b77864a321799bd605f579a4ceed4c13a
Jack Curran <jackcurran49@gmail.com>
2024-07-28 23:23:48 -0400

    updating ebs for costs

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo aws s3 sync /opt/minecraft/server/world s3://minecraft-world-curran/world --delete
+read -p "Is the server shut down? (Y/N): " SHUTDOWN
+if [ "$SHUTDOWN" = "Y" ]; then
+    sudo aws s3 sync /opt/minecraft/server/world s3://minecraft-world-curran/world --delete
+fi
